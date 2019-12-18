533a6bf65cd4e57c32ded55c4f8421f6a67d8a64
Ernesto Nakamura <ernesto@naka.eti.br>
2019-12-18 00:11:37 -0300

    Fix ShippingPriceService

--- woocommerce-jadlog/classes/ShippingPriceService.php
+++ woocommerce-jadlog/classes/ShippingPriceService.php
@@ -20,18 +20,18 @@ class ShippingPriceService {
         $dest_zipcode = str_replace("-", "", $dest_zipcode);
         $request = array(
             'frete' => array(array(
-                'cepori' => $this->cepori,
-                'cepdes' => $dest_zipcode,
-                'frap' => $this->frap,
-                'peso' => $weight,
-                'cnpj' => $this->cnpj,
-                'conta' => $this->conta,
-                'contrato' => $this->contrato,
-                'modalidade' => (int)$this->modalidade,
-                'tpentrega' => $this->tpentrega,
-                'tpseguro' => $this->tpseguro,
+                'cepori'      => $this->cepori,
+                'cepdes'      => $dest_zipcode,
+                'frap'        => $this->frap,
+                'peso'        => $weight,
+                'cnpj'        => $this->cnpj,
+                'conta'       => $this->conta,
+                'contrato'    => $this->contrato,
+                'modalidade'  => (int)$this->modalidade,
+                'tpentrega'   => $this->tpentrega,
+                'tpseguro'    => $this->tpseguro,
                 'vldeclarado' => $declared_value,
-                'vlcoleta' => $this->vlcoleta,
+                'vlcoleta'    => $this->vlcoleta,
             )));
         $params = array(
             'timeout'  => 120,
@@ -47,22 +47,26 @@ class ShippingPriceService {
         error_log('In '.__FUNCTION__.'(), $response = '.var_export($response, true));
 
         $result = array('estimated_value' => null, 'estimated_time' => null);
-        if (is_wp_error($response)) {
-            log_error($response->get_error_message(), __FUNCTION__);
-        } elseif (isset($response['error'])) {
-            log_error($response['error'], __FUNCTION__);
-        } elseif (isset($response['frete'][0]['error'])) {
-            log_error($response['frete'][0]['error'], __FUNCTION__);
-        } else {
+        if (is_wp_error($response))
+            $this->log_error($response->get_error_message(), __FUNCTION__);
+        else {
             $response_body = json_decode($response['body'], true);
-            $result['estimated_value'] = $response_body['frete'][0]['vltotal'];
-            $result['estimated_time']  = $response_body['frete'][0]['prazo'];
+            if (isset($response_body['erro']))
+                $this->log_error($response_body['erro'], __FUNCTION__);
+            elseif (isset($response_body['error']))
+                $this->log_error($response_body['error'], __FUNCTION__);
+            elseif (isset($response_body['frete'][0]['erro']))
+                $this->log_error($response_body['frete'][0]['erro'], __FUNCTION__);
+            else {
+                $result['estimated_value'] = $response_body['frete'][0]['vltotal'];
+                $result['estimated_time']  = $response_body['frete'][0]['prazo'];
+            }
         }
         return $result;
     }
 
     private function log_error($error, $function) {
-        $error_message = '['.$error['id'].'] '.$error['descricao'];
-        error_log('In'.$function.'() ERROR '.$error_message);
+        $error_message = $error['id'].' '.$error['descricao'];
+        error_log('ERROR in '.$function.'(): '.$error_message);
     }
 }
