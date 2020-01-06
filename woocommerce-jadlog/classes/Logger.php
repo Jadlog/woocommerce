7c6e9e1a6d0bcf060b43033606ce9fc31e941dee
Ernesto Nakamura <ernesto@naka.eti.br>
2020-01-05 23:13:30 -0300

    Logger::log_error()

---
+++ woocommerce-jadlog/classes/Logger.php
@@ -0,0 +1,11 @@
+<?php
+class Logger {
+
+    public static function log_error($error, $function, $response = null, $request = null) {
+        if (isset($response))
+            error_log('In '.$function.'(), $response = '.var_export($response, true));
+        if (isset($request))
+            error_log('In '.$function.'(), $request body = '.var_export($request, true));
+        error_log('ERROR in '.$function.'(): '.var_export($error, true));
+    }
+}
