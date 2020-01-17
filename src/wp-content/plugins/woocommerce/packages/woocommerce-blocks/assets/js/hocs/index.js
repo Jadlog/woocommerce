d710c7fdab917282067308be21446dabb5b0cd63
Ernesto Nakamura <ernesto@naka.eti.br>
2020-01-17 14:21:02 -0300

    Deletes src/

--- src/wp-content/plugins/woocommerce/packages/woocommerce-blocks/assets/js/hocs/index.js
+++
@@ -1,3 +0,0 @@
-export { default as withProduct } from './with-product';
-export { default as withCategory } from './with-category';
-export { default as withSearchedProducts } from './with-searched-products';
