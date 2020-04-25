065ae65505b277bd39342a807aba36b180b0bf03
Ernesto Nakamura <ernesto@naka.eti.br>
2020-04-24 23:32:16 -0300

    Fix PudoInfo 'compl2' key not exists warning

--- woocommerce-jadlog/classes/PudoInfo.php
+++ woocommerce-jadlog/classes/PudoInfo.php
@@ -4,45 +4,50 @@ namespace WooCommerce\Jadlog\Classes;
 class PudoInfo {
 
     public function __construct($pudo_data) {
+        require_once("ArrayHelper.php");
+
         $this->_data    = is_array($pudo_data) ? $pudo_data : array();
-        $this->_address = $this->_data['pudoEnderecoList'][0];
+        $address_list = ArrayHelper::safe_get($this->_data, 'pudoEnderecoList', array());
+        $this->_address = ArrayHelper::safe_get($address_list, 0, array());
     }
 
     public function nao_esta_ativo() {
-        return $this->_data['ativo'] != 'S';
+        return !array_key_exists('ativo', $this->_data) ||
+            $this->_data['ativo'] != 'S';
     }
 
     public function get_id() {
-        return $this->_data['pudoId'];
+        return ArrayHelper::safe_get($this->_data, 'pudoId');
     }
 
     public function get_name() {
-        return $this->_data['razao'];
+        return ArrayHelper::safe_get($this->_data, 'razao');
     }
 
     public function get_latitude() {
-        return $this->_address['latitude'];
+        return ArrayHelper::safe_get($this->_address, 'latitude');
     }
 
     public function get_longitude() {
-        return $this->_address['longitude'];
+        return ArrayHelper::safe_get($this->_address, 'longitude');
     }
 
     public function get_postcode() {
-        return $this->_address['cep'];
+        return ArrayHelper::safe_get($this->_address, 'cep');
     }
 
     public function get_openning_hours() {
-        return $this->_data['pudoHorario'];
+        return ArrayHelper::safe_get($this->_data, 'pudoHorario');
     }
 
     public function get_formatted_address() {
-        $address = $this->_address['endereco'];
+        $address = ArrayHelper::safe_get($this->_address, 'endereco');
         if (!empty($this->_address['numero']))
             $address .= ', '.$this->_address['numero'];
         if (!empty($this->_address['compl2']))
             $address .= ' - '.$this->_address['compl2'];
-        if (!empty($this->_address['bairro']) && $this->_address['bairro'] != $this->_address['compl2'])
+        if (!empty($this->_address['bairro']) &&
+            $this->_address['bairro'] != ArrayHelper::safe_get($this->_address, 'compl2'))
             $address .= ' - '.$this->_address['bairro'];
         if (!empty($this->_address['cidade']))
             $address .= ' - '.$this->_address['cidade'];
@@ -57,6 +62,11 @@ class PudoInfo {
     }
 
     public function get_formatted_name_and_address() {
-        return $this->get_name().' ('.$this->get_formatted_address().')';
+        $result = $this->get_name();
+        $address = $this->get_formatted_address();
+        if (!empty($address))
+            $result .= ' ('.$address.')';
+
+        return trim($result);
     }
 }
