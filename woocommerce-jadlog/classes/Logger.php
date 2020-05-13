9e9c044998a96edb819666a1c80c78f0f3e769c4
Ernesto Nakamura <ernesto@naka.eti.br>
2020-05-13 18:43:44 -0300

    Notifies Bugsnag error tracking if plugin is installed

--- woocommerce-jadlog/classes/Logger.php
+++ woocommerce-jadlog/classes/Logger.php
@@ -9,5 +9,20 @@ class Logger {
         if (isset($request))
             error_log('In '.$function.'(), $request body = '.var_export($request, true));
         error_log('ERROR in '.$function.'(): '.var_export($error, true));
+        self::notify_error_tracking($error, $function, $response, $request);
+    }
+
+    public static function notify_error_tracking($error, $function, $response = null, $request = null) {
+        // Bugsnag wordpress plugin must be installed and active.
+        // API key must be configured.
+        global $bugsnagWordpress;
+        if (isset($bugsnagWordpress)) {
+            $message = isset($error['descricao']) ? $error['descricao'] : var_export($error, true);
+            $bugsnagWordpress->notifyError($function, $message, [
+                'error'    => $error,
+                'request'  => $request,
+                'response' => $response
+            ], 'warning');
+        }
     }
 }
